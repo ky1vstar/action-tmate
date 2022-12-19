45abcc2d68361b42337ebaee9a065bc5ba0a12f5
Oleksandr Zhuhan <a.zhuhan@olearis.com>
2022-12-19 15:39:12 +0100

    Use Ocktokit

--- src/output.js
+++ src/output.js
@@ -4,13 +4,17 @@ import * as github from '@actions/github';
 export async function outputSession(token, session) {
   const sha = github.context.sha;
 
-  const client = new github.GitHub(token);
+  const octokit = github.getOctokit(token);
 
   core.debug(`tagging #${sha} with tag ${tag}`);
-  await client.git.createRef({
+  const response = await octokit.rest.git.createRef({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     ref: `refs/tags/${session}`,
     sha: sha
   });
+
+  if (201 !== response.status) {
+    throw `Failed to create tag ref (status=${response.status})`;
+  }
 }
